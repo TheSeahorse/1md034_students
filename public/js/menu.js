e45756a499a7c25b05ef5784f1473b54c9430f2f
Alexander Lahti <alexander.lahti.2359@student.uu.se>
2020-02-12 18:41:28 +0100

    t5-3

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,6 +1,6 @@
 let food = [
     {
-	"name": "The Meat Peak",
+	"name": 'The Meat Peak',
 	"img": "https://i.imgur.com/hlI0BUG.png'",
 	"desc": "A lot of meat (not vegetarian, duh)",
 	"kcal": "Over 9000 cals",
@@ -8,7 +8,7 @@ let food = [
 	"lactose": true,
     },
     {
-	"name": "The Improbable Burger",
+	"name": 'The Improbable Burger',
 	"img": "https://non-gmoreport.com/wp-content/uploads/2019/11/Impossible-burger_transp-800x600.png",
 	"desc": "It\'s vegetarian, we promise",
 	"kcal": "Still a lot of calories though",
@@ -16,7 +16,7 @@ let food = [
 	"lactose": false,
     },
     {
-	"name": "The Barely a Burger Burger",
+	"name": 'The Barely a Burger Burger',
 	"img": "https://www.pngkit.com/png/full/180-1802263_subway-sandwich-png-subway-american-steakhouse-melt.png",
 	"desc": "Does this even count as a burger? (not veggie)",
 	"kcal": "Looks like a sandwich so prob only 100 cals",
@@ -24,7 +24,7 @@ let food = [
 	"lactose": false,
     },
     {
-	"name": "The Whoopsie",
+	"name": 'The Whoopsie',
 	"img": "https://www.stickpng.com/assets/images/5882488ae81acb96424ffaaf.png",
 	"desc": "We didn\'t copy the whooper we promise",
 	"kcal": "645 kcal",
@@ -32,7 +32,7 @@ let food = [
 	"lactose": true,
     },
     {
-	"name": "The Friskis",
+	"name": 'The Friskis',
 	"img": "https://www.gamereactor.se/media/forum/se/16627244_42.jpg",
 	"desc": "We did copy this one from MAX though ngl",
 	"kcal": "789 kcal (I actually don\'t know I\'m just making this shit up)",
--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -4,33 +4,28 @@ const socket = io();
 const vm = new Vue ({
     el: '#main',
     data: {
+	orderInt: 0,
+	details: {x: 0, y: 0},
+	localOrder: {x: -420, y: 0},
 	orders: {},
 	food,
 	burgerCheck: [],
 	burgerDisplay: "",
+	burgerDispatch: [],
 	vname: "",
 	vemail: "",
 	vpayment: "Debit Card",
 	vgenders: "Prefer not to say"
     },
-    created: function() {
-	/* When the page is loaded, get the current orders stored on the server.
-	 * (the server's code is in app.js) */
-	socket.on('initialize', function(data) {
-	    this.orders = data.orders;
-	}.bind(this));
-
-	/* Whenever an addOrder is emitted by a client (every open map.html is
-	 * a client), the server responds with a currentQueue message (this is
-	 * defined in app.js). The message's data payload is the entire updated
-	 * order object. Here we define what the client should do with it.
-	 * Spoiler: We replace the current local order object with the new one. */
-	socket.on('currentQueue', function(data) {
-	    this.orders = data.orders;
-	}.bind(this));
-    },
     methods: {
-	collectOrder: function() {
+	getNext: function() {
+	    /* This function returns the next available key (order number) in
+	     * the orders object, it works under the assumptions that all keys
+	     * are integers. */
+	    this.orderInt += 1;
+	    return this.orderInt;
+	},
+	addOrders: function(event) {
 	    let allInfo = [this.vname, this.vemail, this.vpayment, this.vgenders, this.burgerCheck];
 	    console.log(allInfo);
 	    this.burgerDisplay = "";
@@ -40,34 +35,31 @@ const vm = new Vue ({
 		}
 		this.burgerDisplay += this.burgerCheck[item];
 	    }
-	},
-	getNext: function() {
-	    /* This function returns the next available key (order number) in
-	     * the orders object, it works under the assumptions that all keys
-	     * are integers. */
-	    let lastOrder = Object.keys(this.orders).reduce(function(last, next) {
-		return Math.max(last, next);
-	    }, 0);
-	    return lastOrder + 1;
-	},
-	addOrder: function(event) {
-	    /* When you click in the map, a click event object is sent as parameter
-	     * to the function designated in v-on:click (i.e. this one).
-	     * The click event object contains among other things different
-	     * coordinates that we need when calculating where in the map the click
-	     * actually happened. */
+	    // When you click in the map, a click event object is sent as parameter
+	    //to the function designated in v-on:click (i.e. this one).
+	    //The click event object contains among other things different
+	    //coordinates that we need when calculating where in the map the click
+	    //actually happened.
 	    let offset = {
 		x: event.currentTarget.getBoundingClientRect().left,
 		y: event.currentTarget.getBoundingClientRect().top,
 	    };
 	    socket.emit('addOrder', {
 		orderId: this.getNext(),
-		details: {
-		    x: event.clientX - 10 - offset.x,
-		    y: event.clientY - 10 - offset.y,
-		},
-		orderItems: ['Beans', 'Curry'],
+		details: this.localOrder,
+		orderItems: this.burgerCheck,
 	    });
+	},
+	displayOrder: function(event) {
+	    
+	    let offset = {
+		x: event.currentTarget.getBoundingClientRect().left,
+		y: event.currentTarget.getBoundingClientRect().top,
+	    };
+	    this.localOrder = {
+		x: event.clientX - 10 - offset.x,
+		y: event.clientY - 10 - offset.y,
+	    }
 	},
     }
     
